e7eeda11437fe542f04788afd539e4c79654dfc5
Yohanes Prasdya <114075476+yohanCod3@users.noreply.github.com>
2026-01-21 22:44:40 +0700

    First Practicum Ever

---
+++ Praktikum 1/tugas3.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    int angka2 = 10;            // Nama variabel tidak boleh diawali angka
+    float tinggi_badan = 170.5; // Gunakan titik (.) untuk desimal, bukan koma
+    char nama_lengkap = 'B';    // Gunakan tanda kutip tunggal yang benar
+
+    return 0;
+}
